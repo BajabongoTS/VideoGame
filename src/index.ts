543b49f78df8d9a22d4791259c69ee189926f02f
Konrad Klautzsch <konradklautzsch@gmail.com>
2025-04-05 11:28:11 +0200

    naprawienie błędów

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {gameRouter} from "./game/gameRouter";
+import express,{response, type Request, type Response, type NextFunction} from 'express';
+import { gameRouter } from "./game/gameRouter";
+
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(bodyParser.json())
 app.use('/games', gameRouter);
+
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
